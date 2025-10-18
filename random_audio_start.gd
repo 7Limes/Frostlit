a539b1967a388a4454f73bf6fc3aa810d6fa1ece
Miles Burkart <milesb2288@gmail.com>
2025-10-17 21:46:08 -0400

    Add ambient wind sound

---
+++ random_audio_start.gd
@@ -0,0 +1,10 @@
+extends AudioStreamPlayer
+
+func _ready():
+	play()
+	# Wait one frame to ensure the stream is properly initialized
+	await get_tree().process_frame
+	
+	var stream_length = stream.get_length()
+	var random_position = randf() * stream_length
+	seek(random_position)
